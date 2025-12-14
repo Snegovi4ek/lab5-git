57159d8ec6de320fc1a9346dd2eabe5faf7a3c3d
Snegovi4ek <sergeymayakov46@gmail.com>
2025-12-15 02:30:06 +0300

    Второй коммит в ветке dev

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
     public static void main(String[] args) {
         System.out.print("Первое изменение в ветке dev");
+        System.out.print("Второе изменение в ветке dev");
     }
 }
